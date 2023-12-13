71bccd18d26ee9213d0e31f7715fe87bdf830572
Jason Chow <jchow149@my.bcit.ca>
2023-12-12 16:29:11 -0800

    Add display pokemon

--- public/script.js
+++ public/script.js
@@ -9,33 +9,63 @@ async function getPokemon() {
         console.log(`Response status: ${response.status}`);
 
         if (response.status === 200) {
-            const pokemon = await response.json()
-            displayPokemon(pokemon)
+            const pokemonResp = await response.json()
+            displayPokemon(pokemonResp)
         }
     } catch (error) {
         console.error(`Error due to ${error}`)
     }
 }
 
-async function displayPokemon(pokemon) {
+async function displayPokemon(pokemonResp) {
     const pokemonContainer = document.getElementById("pokemonContainer");
     pokemonContainer.innerHTML = "";
 
     try {
-        if (pokemon.length <= 0) {
+        if (pokemonResp.length <= 0) {
             const noPokemon = document.createElement("p");
-            noPokemon.innerHTML = "<p id='noPokemon'>No Pokemon found with those attributes!</p>"
+            noPokemon.textContent = "No Pokemon found with those attributes!";
             pokemonContainer.appendChild(noPokemon);
             return;
         }
-        pokemon.innerHTML +=
-            `<div>
-        <img src=${pokemon.image.thumbnail}
-        <p>${pokemon.name}</p>
-        <p>${pokemon.type}</p>
-        </div>`
 
+        for (let i = 0; i < pokemonResp.length; i++) {
+            const pokemon = pokemonResp[i];
+            const pokemonDiv = document.createElement("div");
+
+            // Image
+            const pokemonImage = document.createElement("img");
+            pokemonImage.src = pokemon.image.thumbnail; // Adjust according to your JSON structure
+            pokemonImage.alt = `Image of ${pokemon.name.english}`; // Use the English name for alt text
+            pokemonDiv.appendChild(pokemonImage);
+
+            // Name (English)
+            const pokemonName = document.createElement("p");
+            pokemonName.textContent = pokemon.name.english; // Displaying the English name
+            pokemonDiv.appendChild(pokemonName);
+
+            // Type
+            const pokemonType = document.createElement("p");
+            pokemonType.textContent = `Type: ${pokemon.type.join(', ')}`; // Joining the type array into a string
+            pokemonDiv.appendChild(pokemonType);
+
+            // Description
+            const pokemonDescription = document.createElement("p");
+            pokemonDescription.textContent = pokemon.description; // Adding description
+            pokemonDiv.appendChild(pokemonDescription);
+
+            pokemonContainer.appendChild(pokemonDiv);
+        }
     } catch (error) {
-        console.error(`Error due to ${error}`)
+        console.error(`Error due to ${error}`);
     }
 }
+
+async function main() {
+    const response = await fetch("/pokemon");
+    const pokemonResp = await response.json();
+    // Displays all unicorns first
+    displayPokemon(pokemonResp);
+}
+
+main().catch((error) => console.error(`Error due to ${error}`));
